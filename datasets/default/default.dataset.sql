bf510eec539d4376d727c55f42da62f4df41e2a4
athorthi <athorthi@salesforce.com>
2023-05-28 11:42:49 +0530

    Pushing local files for metecho testing

--- datasets/default/default.dataset.sql
+++ datasets/default/default.dataset.sql
@@ -1,81 +1,17 @@
 BEGIN TRANSACTION;
 CREATE TABLE "Account" (
 	id INTEGER NOT NULL, 
-	"AccountNumber" VARCHAR(255), 
-	"AccountSource" VARCHAR(255), 
-	"AnnualRevenue" VARCHAR(255), 
-	"BillingCity" VARCHAR(255), 
-	"BillingCountry" VARCHAR(255), 
-	"BillingGeocodeAccuracy" VARCHAR(255), 
-	"BillingLatitude" VARCHAR(255), 
-	"BillingLongitude" VARCHAR(255), 
-	"BillingPostalCode" VARCHAR(255), 
-	"BillingState" VARCHAR(255), 
-	"BillingStreet" VARCHAR(255), 
-	"Description" VARCHAR(255), 
-	"Fax" VARCHAR(255), 
-	"FirstName" VARCHAR(255), 
-	"Industry" VARCHAR(255), 
-	"Jigsaw" VARCHAR(255), 
-	"LastName" VARCHAR(255), 
 	"Name" VARCHAR(255), 
-	"NumberOfEmployees" VARCHAR(255), 
-	"Ownership" VARCHAR(255), 
-	"PersonAssistantName" VARCHAR(255), 
-	"PersonAssistantPhone" VARCHAR(255), 
-	"PersonBirthdate" VARCHAR(255), 
-	"PersonDepartment" VARCHAR(255), 
-	"PersonDoNotCall" VARCHAR(255), 
-	"PersonEmail" VARCHAR(255), 
-	"PersonEmailBouncedDate" VARCHAR(255), 
-	"PersonEmailBouncedReason" VARCHAR(255), 
-	"PersonHasOptedOutOfEmail" VARCHAR(255), 
-	"PersonHasOptedOutOfFax" VARCHAR(255), 
-	"PersonHomePhone" VARCHAR(255), 
-	"PersonLeadSource" VARCHAR(255), 
-	"PersonMailingCity" VARCHAR(255), 
-	"PersonMailingCountry" VARCHAR(255), 
-	"PersonMailingGeocodeAccuracy" VARCHAR(255), 
-	"PersonMailingLatitude" VARCHAR(255), 
-	"PersonMailingLongitude" VARCHAR(255), 
-	"PersonMailingPostalCode" VARCHAR(255), 
-	"PersonMailingState" VARCHAR(255), 
-	"PersonMailingStreet" VARCHAR(255), 
-	"PersonMobilePhone" VARCHAR(255), 
-	"PersonOtherCity" VARCHAR(255), 
-	"PersonOtherCountry" VARCHAR(255), 
-	"PersonOtherGeocodeAccuracy" VARCHAR(255), 
-	"PersonOtherLatitude" VARCHAR(255), 
-	"PersonOtherLongitude" VARCHAR(255), 
-	"PersonOtherPhone" VARCHAR(255), 
-	"PersonOtherPostalCode" VARCHAR(255), 
-	"PersonOtherState" VARCHAR(255), 
-	"PersonOtherStreet" VARCHAR(255), 
-	"PersonTitle" VARCHAR(255), 
+	"BillingState" VARCHAR(255),
 	"Phone" VARCHAR(255), 
-	"Rating" VARCHAR(255), 
-	"Salutation" VARCHAR(255), 
-	"ShippingCity" VARCHAR(255), 
-	"ShippingCountry" VARCHAR(255), 
-	"ShippingGeocodeAccuracy" VARCHAR(255), 
-	"ShippingLatitude" VARCHAR(255), 
-	"ShippingLongitude" VARCHAR(255), 
-	"ShippingPostalCode" VARCHAR(255), 
-	"ShippingState" VARCHAR(255), 
-	"ShippingStreet" VARCHAR(255), 
-	"Sic" VARCHAR(255), 
-	"SicDesc" VARCHAR(255), 
-	"Site" VARCHAR(255), 
-	"TickerSymbol" VARCHAR(255), 
 	"Type" VARCHAR(255), 
-	"Website" VARCHAR(255), 
 	"IsPersonAccount" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Account" VALUES(1,'','','','','','','','','','','','','','John','','','Marshal','','','','','','','','False','','','','False','False','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','True');
-INSERT INTO "Account" VALUES(2,'','','100000000.0','New York','USA','','','','31349','NY','10 Main Rd.','','(212) 555-5555','','Manufacturing','','','Acme','680','','','','','','False','','','','False','False','','','','','','','','','','','','','','','','','','','','','','(212) 555-5555','','','New York','USA','','','','31349','NY','10 Main Rd.','','','','','Prospect','','False');
-INSERT INTO "Account" VALUES(3,'','','','San Francisco','USA','','','','94105','CA','The Landmark @ One Market, Suite 300','Founded in March 1999, salesforce.com (http://www.salesforce.com) builds and delivers customer relationship management (CRM) applications as scalable online services. The salesforce.com product suite - Team Edition, Professional Edition, Enterprise Edition, Wireless Edition and Offline Edition - gives companies of all sizes a complete 360-degree view of the customer. The company''s award-winning CRM solutions provide integrated online sales force automation, customer service and support management, and marketing automation applications to help companies meet the complex challenges of global customer communication. Salesforce.com has received considerable recognition in the industry, including Editors'' Choice and two Five-Star ratings from PC Magazine, two Deploy Awards from InfoWorld, Red Herring 100, Upside Hot 100, Investor''s Choice Award from Enterprise Outlook, Editors'' Choice from TMCLabs, Top 10 CRM Implementation from Aberdeen Group, and InfoWorld''s 2001 CRM Technology of the Year. Founded in 1999, salesforce.com is headquartered in San Francisco, with offices in Europe and Asia.','(415) 901-7040','','Technology','','','salesforce.com','','','','','','','False','','','','False','False','','','','','','','','','','','','','','','','','','','','','','(415) 901-7000','','','San Francisco','USA','','','','94105','CA','The Landmark @ One Market, Suite 300','','','','','Customer','http://www.salesforce.com','False');
-INSERT INTO "Account" VALUES(4,'','','','Toronto','Canada','','','','L4B 1Y3','Ontario','150 Chestnut Street','GBM is the worldwide leader in technology news and information on the Web and the producer of the longest-running and farthest-reaching television shows about technology. GBM''s network of sites combines breakthrough interactive technology with engaging content and design and is consistently ranked as the Internet''s leading content network in terms of both audience size and revenue, serving millions of users each day. The company''s television programming is broadcast by the USA Network and the Sci-Fi Channel to more than 70 million households and is syndicated to broadcast television stations in the nation''s top 120 markets, including the top 10 markets.','','','Media','','','Global Media','14668','','','','','','False','','','','False','False','','','','','','','','','','','','','','','','','','','','','','(905) 555-1212','','','Toronto','Canada','','','','L4B 1Y3','Ontario','150 Chestnut Street','','','','','Prospect','','False');
+INSERT INTO "Account" VALUES(1,'John','NY','','','True');
+INSERT INTO "Account" VALUES(2,'Acme','NY','(212) 555-5555','Prospect','False');
+INSERT INTO "Account" VALUES(3,'salesforce.com','CA','(415) 901-7000','Customer','False');
+INSERT INTO "Account" VALUES(4,'Global Media','Ontario','(905) 555-1212','Prospect','False');
 CREATE TABLE "AssessmentQuestion" (
 	id INTEGER NOT NULL, 
 	"QuestionCategory" VARCHAR(255), 
@@ -88,10 +24,16 @@ CREATE TABLE "AssessmentQuestion" (
 );
 INSERT INTO "AssessmentQuestion" VALUES(1,'Demographic','Text','Sample_Assessment_Question_2','','Sample Assessment Question 1','');
 INSERT INTO "AssessmentQuestion" VALUES(2,'Financial','Currency','Sample_Assessment_Question','','Sample Assessment Question','');
+CREATE TABLE "Program" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"StartDate" VARCHAR(255),
+	PRIMARY KEY (id)
+);
+INSERT INTO "Program" VALUES(1,'Sample Program','2023-05-18');
 CREATE TABLE "Benefit" (
 	id INTEGER NOT NULL, 
 	"IsActive" VARCHAR(255), 
-	"BenefitFrequency" VARCHAR(255), 
 	"Name" VARCHAR(255), 
 	"BenefitStatus" VARCHAR(255), 
 	"Description" VARCHAR(255), 
@@ -99,22 +41,32 @@ CREATE TABLE "Benefit" (
 	"EnrollmentCount" VARCHAR(255), 
 	"MaxBenefitAmount" VARCHAR(255), 
 	"MinBenefitAmount" VARCHAR(255), 
-	"StartDateTime" VARCHAR(255), 
+	"StartDateTime" VARCHAR(255),  
 	"BenefitTypeId" VARCHAR(255), 
 	"GoalDefinitionId" VARCHAR(255), 
 	"UnitId" VARCHAR(255), 
+	"ProgramId" VARCHAR(255),
 	PRIMARY KEY (id)
 );
-INSERT INTO "Benefit" VALUES(1,'True','','Sample Benefit','','','','','','','','1','','1');
+INSERT INTO "Benefit" VALUES(1,'True','Sample Benefit','','','','','','','','1','','1','1');
+CREATE TABLE "UnitOfMeasure" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"Type" VARCHAR(255), 
+	"UnitCode" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "UnitOfMeasure" VALUES(1,'UOM','Period','UOM');
 CREATE TABLE "BenefitType" (
 	id INTEGER NOT NULL, 
 	"Name" VARCHAR(255), 	 
 	"Category" VARCHAR(255), 
 	"Description" VARCHAR(255), 
 	"ProcessType" VARCHAR(255), 
+	"UnitofMeasureId" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "BenefitType" VALUES(1,'Sample Benefit Type','None','','PublicSector');
+INSERT INTO "BenefitType" VALUES(1,'Sample Benefit Type','None','','PublicSector','1');
 CREATE TABLE "CaseParticipant" (
 	id INTEGER NOT NULL, 
 	"Role" VARCHAR(255), 
@@ -191,14 +143,11 @@ CREATE TABLE "Contact" (
 	"Birthdate" VARCHAR(255), 
 	"Department" VARCHAR(255), 
 	"Description" VARCHAR(255), 
-	"DoNotCall" VARCHAR(255), 
 	"Email" VARCHAR(255), 
 	"EmailBouncedDate" VARCHAR(255), 
 	"EmailBouncedReason" VARCHAR(255), 
 	"Fax" VARCHAR(255), 
-	"FirstName" VARCHAR(255), 
-	"HasOptedOutOfEmail" VARCHAR(255), 
-	"HasOptedOutOfFax" VARCHAR(255), 
+	"FirstName" VARCHAR(255),
 	"HomePhone" VARCHAR(255), 
 	"Jigsaw" VARCHAR(255), 
 	"LastName" VARCHAR(255), 
@@ -227,14 +176,14 @@ CREATE TABLE "Contact" (
 	"IsPersonAccount" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Contact" VALUES(1,'','','','','','False','','','','','John','False','False','','','Marshal','','','','','','','','','','','','','','','','','','','','','','','True');
-INSERT INTO "Contact" VALUES(2,'','','','','','False','info@salesforce.com','','','(212) 555-5555','Edward','False','False','','','Stamos','','New York City','USA','','','','31349','NY','10 Main Rd.','','New York City','USA','','','','','31349','NY','10 Main Rd.','(212) 555-5555','','President and CEO','False');
-INSERT INTO "Contact" VALUES(3,'','','','','','False','info@salesforce.com','','','(212) 555-5555','Howard','False','False','','','Jones','','New York','USA','','','','31349','NY','10 Main Rd.','','','','','','','','','','','(212) 555-5555','','Buyer','False');
-INSERT INTO "Contact" VALUES(4,'','','','','','False','info@salesforce.com','','','(212) 555-5555','Leanne','False','False','','','Tomlin','','New York','USA','','','','31349','NY','10 Main Rd.','','','','','','','','','','','(212) 555-5555','','VP Customer Support','False');
-INSERT INTO "Contact" VALUES(5,'','','','','Marc R. Benioff is chairman and CEO of salesforce.com. He founded the company in March 1999 with the idea to create an information utility that would make traditional enterprise software technology and business models obsolete. A veteran of 25 years in the software industry, he is now regarded as a pioneer of "The End Of Software," demonstrating how on-demand applications can replace traditional software to deliver immediate benefit at reduced risk. In May 2003, Benioff was appointed by President George W. Bush as co-chair of the President''s Information Technology Advisory Committee (PITAC), a bi-partisan organization of business leaders and academics charged to advise The President on how to maintain the United States'' preeminent position in information technology.','False','info@salesforce.com','','','(415) 901-7040','Marc','False','False','','','Benioff','','San Francisco','USA','','','','94105','CA','The Landmark @ One Market, Suite 300','','San Francisco','USA','','','','','94105','CA','The Landmark @ One Market, Suite 300','(415) 901-7000','Mr.','Executive Officer','False');
-INSERT INTO "Contact" VALUES(6,'','','','','','False','info@salesforce.com','','','','Geoff','False','False','','','Minor','','Toronto','Canada','','','','L4B 1Y3','Ontario','150 Chestnut Street','','Toronto','Canada','','','','','L4B 1Y3','Ontario','150 Chestnut Street','(415) 555-1212','','President','False');
-INSERT INTO "Contact" VALUES(7,'','','','','Carole White is looking at moving forward with our products and will act as the champion for us.','False','info@salesforce.com','','','','Carole','False','False','','','White','Employee Referral','Toronto','Canada','','','','L4B 1Y3','Ontario','150 Chestnut Street','','','','','','','','','','','(415) 555-1212','','VP Sales','False');
-INSERT INTO "Contact" VALUES(8,'','','','','','False','info@salesforce.com','','','(555) 555-1212','Jon','False','False','','','Amos','','Toronto','Canada','','','','L4B 1Y3','Ontario','150 Chestnut Street','','','','','','','','','','','(905) 555-1212','','Sales Manager','False');
+INSERT INTO "Contact" VALUES(1,'','','','','','','','','','John','','','Marshal','','','','','','','','','','','','','','','','','','','','','','','True');
+INSERT INTO "Contact" VALUES(2,'','','','','','info@salesforce.com','','','(212) 555-5555','Edward','','','Stamos','','New York City','USA','','','','31349','NY','10 Main Rd.','','New York City','USA','','','','','31349','NY','10 Main Rd.','(212) 555-5555','','President and CEO','False');
+INSERT INTO "Contact" VALUES(3,'','','','','','info@salesforce.com','','','(212) 555-5555','Howard','','','Jones','','New York','USA','','','','31349','NY','10 Main Rd.','','','','','','','','','','','(212) 555-5555','','Buyer','False');
+INSERT INTO "Contact" VALUES(4,'','','','','','info@salesforce.com','','','(212) 555-5555','Leanne','','','Tomlin','','New York','USA','','','','31349','NY','10 Main Rd.','','','','','','','','','','','(212) 555-5555','','VP Customer Support','False');
+INSERT INTO "Contact" VALUES(5,'','','','','Marc R. Benioff is chairman and CEO of salesforce.com. He founded the company in March 1999 with the idea to create an information utility that would make traditional enterprise software technology and business models obsolete. A veteran of 25 years in the software industry, he is now regarded as a pioneer of "The End Of Software," demonstrating how on-demand applications can replace traditional software to deliver immediate benefit at reduced risk. In May 2003, Benioff was appointed by President George W. Bush as co-chair of the President''s Information Technology Advisory Committee (PITAC), a bi-partisan organization of business leaders and academics charged to advise The President on how to maintain the United States'' preeminent position in information technology.','info@salesforce.com','','','(415) 901-7040','Marc','','','Benioff','','San Francisco','USA','','','','94105','CA','The Landmark @ One Market, Suite 300','','San Francisco','USA','','','','','94105','CA','The Landmark @ One Market, Suite 300','(415) 901-7000','Mr.','Executive Officer','False');
+INSERT INTO "Contact" VALUES(6,'','','','','','info@salesforce.com','','','','Geoff','','','Minor','','Toronto','Canada','','','','L4B 1Y3','Ontario','150 Chestnut Street','','Toronto','Canada','','','','','L4B 1Y3','Ontario','150 Chestnut Street','(415) 555-1212','','President','False');
+INSERT INTO "Contact" VALUES(7,'','','','','Carole White is looking at moving forward with our products and will act as the champion for us.','info@salesforce.com','','','','Carole','','','White','Employee Referral','Toronto','Canada','','','','L4B 1Y3','Ontario','150 Chestnut Street','','','','','','','','','','','(415) 555-1212','','VP Sales','False');
+INSERT INTO "Contact" VALUES(8,'','','','','','info@salesforce.com','','','(555) 555-1212','Jon','','','Amos','','Toronto','Canada','','','','L4B 1Y3','Ontario','150 Chestnut Street','','','','','','','','','','','(905) 555-1212','','Sales Manager','False');
 CREATE TABLE "GoalDefinition" (
 	id INTEGER NOT NULL, 
 	"Category" VARCHAR(255), 
@@ -246,12 +195,4 @@ CREATE TABLE "GoalDefinition" (
 	PRIMARY KEY (id)
 );
 INSERT INTO "GoalDefinition" VALUES(1,'','','Sample Goal Definition','Active','Individual','');
-CREATE TABLE "UnitOfMeasure" (
-	id INTEGER NOT NULL, 
-	"Name" VARCHAR(255), 
-	"Type" VARCHAR(255), 
-	"UnitCode" VARCHAR(255), 
-	PRIMARY KEY (id)
-);
-INSERT INTO "UnitOfMeasure" VALUES(1,'UOM','Period','UOM');
 COMMIT;
